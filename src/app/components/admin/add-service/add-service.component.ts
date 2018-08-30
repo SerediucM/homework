3978afd3fe31d97c250649b1677af52885b6ff7a
SerediucM <intern.mihaela.serediuc@assist.ro>
2018-08-30 17:07:42 +0300

    add image account --next object in object calendar

--- src/app/components/admin/add-service/add-service.component.ts
+++ src/app/components/admin/add-service/add-service.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-service.component.css']
 })
 export class AddServiceComponent implements OnInit {
-
+  private Luni7: string;
+  private Luni8: string;
+  private Luni9: string;
+  private Luni10: string;
   constructor() { }
 
   ngOnInit() {
   }
+test(){
+  console.log(" aici  "+ this.Luni7 +" aici2  "+ this.Luni8 )
+}
 
 }
--- src/app/components/admin/my-account/my-account.component.ts
+++ src/app/components/admin/my-account/my-account.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-account.component.css']
 })
 export class MyAccountComponent implements OnInit {
-
+  companyName:string;
+  public file: File;
   constructor() { }
 
   ngOnInit() {
   }
+  handleFileSelect(evt) {
+    var files = evt.target.files;
+    this.file = files[0];
+    console.log(this.file);
 
+    if (files && this.file) {
+      var reader = new FileReader();
+
+      reader.onload = this._handleReaderLoaded.bind(this);
+
+      reader.readAsBinaryString(this.file);
+    }
+  }
+  _handleReaderLoaded(readerEvt) {
+    var binaryString = readerEvt.target.result;
+    binaryString = btoa(binaryString);
+    console.log(btoa(binaryString));
+  }
 }
